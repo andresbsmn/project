e9db2a31aa1607dbc61c3f600788185916a0d539
andresbsmn <andres.bosman@gmail.com>
2022-10-02 13:25:34 +0200

    test

--- sdltest.py
+++ sdltest.py
@@ -1,22 +1,34 @@
-import sys
 import sdl2
 import sdl2.ext
-RESOURCES = sdl2.ext.Resources(__file__, "resources")
 
 
 def run():
     sdl2.ext.init()
     window = sdl2.ext.Window("test", size=(800, 600))
+    renderer = sdl2.ext.Renderer(window)
+    resources = sdl2.ext.Resources(__file__, "resources")
+    factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE, renderer=renderer)
+    wall_texture = factory.from_image(resources.get_path("wood.png"))
     window.show()
     running = True
     while running:
         events = sdl2.ext.get_events()
 
-        factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-        spriterenderer.render(sprite)
-        sprite = factory.from_image(RESOURCES.get_path("hello.bmp"))
 
-        spriterenderer = factory.create_sprite_render_system(window)
+        renderer.present()
+
+
+        breedte = wall_texture.size[0]
+        hoogte = wall_texture.size[1]
+        textuur_x = 0
+        textuur_y = 0
+        scherm_x = 10
+        scherm_y = 100
+        renderer.copy(wall_texture,
+                      srcrect=(textuur_x, textuur_y, breedte, hoogte),
+                      dstrect=(scherm_x, scherm_y, breedte*4, hoogte*4))
+
+        window.refresh()
         for event in events:
             if event.type == sdl2.SDL_QUIT:
                 running = False
@@ -24,6 +36,5 @@ def run():
         window.refresh()
     return 0
 
-if __name__ == "__main__":
-    sys.exit(run())
+run()
 
