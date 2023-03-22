6a60a36595d66c7ae8fea8eb3f5b26c7a66d3093
feniksdesmet <feniksdesmet@yahoo.com>
2023-03-22 17:02:35 +0100

    test unit Feniks voor testen standaard fcts controller buiten python.

---
+++ TestUnit bakske.py
@@ -0,0 +1,38 @@
+import serial
+import time
+
+def vibrator():
+    ser = serial.Serial('COM9', 9600, timeout=1)  # open serial port
+    ser.write(b'v')
+    time.sleep(1)
+    ser.write(b'0')
+    ser.close()
+
+def buzzer():
+    ser = serial.Serial('COM9', 9600, timeout=1)  # open serial port  (com7 desktop Feniks,COM9 laptop Feniks)
+    ser.write(b'b')
+    time.sleep(1)
+    ser.write(b'0')
+    ser.close()
+
+
+
+
+
+
+for i in range(5):
+    print(i)
+print("Buzzer wordt geactiveerd! ")
+buzzer()
+
+print("buzzer deactiveerd")
+
+
+
+for i in range(5):
+    print(i)
+
+print("vibrator active")
+vibrator()
+
+print("deactive ")
