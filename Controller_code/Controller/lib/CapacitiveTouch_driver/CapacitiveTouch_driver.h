73039a1949cd27b3e0a1bb3ac824e1e0b4bab0d7
Feniks <feniksdesmet@yahoo.com>
2023-04-22 16:24:36 +0200

    Code van de controller

---
+++ Controller_code/Controller/lib/CapacitiveTouch_driver/CapacitiveTouch_driver.h
@@ -0,0 +1,14 @@
+//capacitive touch driver 
+
+#ifndef CapacitiveTouch_driver_h
+#define CapacitiveTouch_driver_h
+#include <Arduino.h>
+#include "HID-Project.h"
+
+const int CapacitivePin = 22;
+const int pinLED=13;
+void capacitive_ini();
+void save();
+
+
+#endif
