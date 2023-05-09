fb7e4b59905fc71660d2a6333f391aba1db19b59
andresbsmn <114671217+andresbsmn@users.noreply.github.com>
2023-05-09 12:14:50 +0200

    Delete Controller directory

--- Controller/lib/Buig_Driver/Buig_Driver.h
+++
@@ -1,8 +0,0 @@
-#ifndef Buig_Driver_h
-#define Buig_Driver_h
-#include <Arduino.h>
-#include "HID-Project.h"
-const int pinFlex=18;
-void buigsensor();
-
-#endif
--- Controller/lib/Buzzerdriver/Buzzerdriver.h
+++
@@ -1,7 +0,0 @@
-#ifndef Buzzerdriver_h
-#define Buzzerdriver_h
-const int buzzer=2; //pin van buzzer
-void buzzer_begin();   // initialiseert buzzer
-void buzzer_push(long duration,int frequency);
-
-#endif
